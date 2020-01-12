6fb74550db1d310cedb2c270f2ee46a42c674061
Andre Weltsch <weltsch@zib.de>
2020-01-12 17:25:16 +0100

    Read file contents.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::env;
-
-fn runFile(path: &str) {
-}
-
-fn runPrompt() {
-}
+use std::error::Error;
+use std::fs::File;
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,10 +12,31 @@ fn main() {
         println!("Usage: rlox [script]");
         std::process::exit(64);
     } else if args.len() == 2 {
-        runFile(&args[1]);
+        let path = Path::new(&args[1]);
+        runFile(path);
     } else if args.len() == 1 {
         runPrompt();
     } else {
         panic!("Fatal: there needs to be at least one argument!");
     }
+}
+
+fn runFile(path: &Path) {
+    let mut file = match File::open(path) {
+        Err(why) => panic!("can't open {}: {}", path.display(),
+                                                why.description()),
+        Ok(file) => file
+    };
+
+    let fileContent = match fs::read_to_string(path) {
+        Err(why) => panic!("can not read file conents: {}", why.description()),
+        Ok(s) => s
+    };
+    run(&fileContent);
+}
+
+fn runPrompt() {
+}
+
+fn run(s: &str) {
 }
