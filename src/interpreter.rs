afb9144284592bc560f6729b90ecffb587761bda
Andre Weltsch <weltsch@zib.de>
2020-04-13 22:53:35 +0200

    Add environment to interpreter.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,14 +1,35 @@
+use std::collections::HashMap;
+
 use crate::parser::Statement;
 use crate::parser::ParseError;
 use crate::eval::evaluate;
 use crate::eval::LoxValue;
 
 pub struct Interpreter {
+    environment: Environment
+}
+
+pub struct Environment {
+    variable_map: HashMap<String, LoxValue>
+}
+
+impl Environment {
+    pub fn new() -> Self {
+        Environment { variable_map: HashMap::new() }
+    }
+
+    pub fn define(&mut self, name: &str, value: LoxValue) {
+        self.variable_map.insert(name.to_string(), value);
+    }
+
+    pub fn get(&self, name: &str) -> Result<&LoxValue, String> {
+        self.variable_map.get(name).ok_or_else(|| format!("Undefined variable {}.", name))
+    }
 }
 
 impl Interpreter {
     pub fn new() -> Interpreter {
-        Interpreter { }
+        Interpreter { environment: Environment::new() }
     }
 
     pub fn interpret(&self, stmts: &Vec<Statement>) {
